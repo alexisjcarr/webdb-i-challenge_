75e4968230b2f65db2a8d3af2137449bb4de7bb2
alexisjcarr <acarr59@gmail.com>
2019-07-15 15:09:05 -0500

    ✅ Adds query string capability for GET /accounts (stretch).
     #1

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -3,8 +3,12 @@ const db = require("../data/dbConfig");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
+  const { limit = 5, sortBy = "id", sortDir = "asc" } = req.query;
+
   try {
-    const accts = await db("accounts");
+    const accts = await db("accounts")
+      .orderBy(sortBy, sortDir)
+      .limit(limit);
     res.status(200).json(accts);
   } catch (err) {
     res.status(500).json(err.message);
@@ -14,8 +18,14 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const acct = await db("accounts").where({ id: id });
-    res.status(200).json(acct);
+    const acct = await db("accounts")
+      .where({ id: id })
+      .first();
+    if (!acct) {
+      res.status(400).json({ message: `No account with id ${id}.` });
+    } else {
+      res.status(200).json(acct);
+    }
   } catch (err) {
     res.status(500).json(err.message);
   }
